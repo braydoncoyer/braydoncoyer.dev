adf0e425b0b1de824564151f137f678e9f821465
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-10-22 11:40:36 -0500

    Added Inter font and integrated with Tailwind (#3)

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,28 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Inter',
+            variants: [
+              '100',
+              '200',
+              '300',
+              '400',
+              '500',
+              '600',
+              '700',
+              '800',
+              '900',
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
