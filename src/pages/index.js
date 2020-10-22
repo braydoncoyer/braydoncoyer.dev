129bd956727e2e11432e0d001d83ab3d9a3f3f6c
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-10-22 17:21:22 -0500

    Added image, and first draft of welcome screen complete (#5)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Memoji from '../assets/braydon_memoji.jpeg';
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <h1 className="text-6xl font-semibold text-primary">Hi, I'm Braydon</h1>
-        <h4 className="text-base text-secondary">
-          I'm a senior full stack engineer with a passion for everything
-          front-end
-        </h4>
+      <div className="flex justify-center items-center min-h-screen -mt-16">
+        <div className="flex flex-col items-center space-y-8 md:flex-row-reverse md:justify-between">
+          <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" />
+          <div className="text-center space-y-2 md:text-left">
+            <h1 className="text-4xl font-semibold text-primary md:text-6xl">
+              Hi, I'm <mark>Braydon</mark> 👋
+            </h1>
+            <p className="text-base font-thin text-secondary md:text-2xl">
+              I'm a senior full stack engineer <br />
+              with a passion for everything front-end
+            </p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
