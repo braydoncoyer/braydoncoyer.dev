c52d8f884e636e32440bedfe269e463a1b039475
Braydon Coyer <braydon@coyer.net>
2020-10-22 12:07:12 -0500

    Adding dark/light mode toggle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from '../helpers/themeContext';
+import { Toggle } from '../components/toggle';
 
 export default function Home() {
   return (
-    <div className="mt-0">
-      <h1 className="text-6xl font-semibold">Hi, I'm Braydon</h1>
-      <h4 className="text-base">
-        I'm a senior full stack engineer with a passion for everything front-end
-      </h4>
-    </div>
+    <ThemeProvider>
+      <Toggle />
+      <div className="mt-0 bg-primary text-primary">
+        <h1 className="text-6xl font-semibold">Hi, I'm Braydon</h1>
+        <h4 className="text-base">
+          I'm a senior full stack engineer with a passion for everything
+          front-end
+        </h4>
+        <div>some content here</div>
+      </div>
+    </ThemeProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+      },
+      textColor: {
+        accent: 'var(--color-text-accent)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+      },
+      colors: {
+        'dark-mode-blue': '#171923',
+      },
+    },
     fontFamily: {
       inter: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
     },
