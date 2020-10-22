e665fca044e1f1c76ca0caaa0468b9b24ab56541
Braydon Coyer <braydon@coyer.net>
2020-10-22 18:05:13 -0500

    Removed negative margin to fix full-screen welcome issue

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Memoji from '../assets/braydon_memoji.jpeg';
 
 const WelcomeSection = () => (
-  <div className="flex justify-center items-center min-h-screen -mt-16">
+  <div className="min-h-screen flex justify-center items-center">
     <div className="flex flex-col items-center space-y-8 md:flex-row-reverse md:justify-between">
       <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" />
       <div className="text-center space-y-2 md:text-left">
