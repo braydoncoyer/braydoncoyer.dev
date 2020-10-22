37dca54b0a816dbc571c2533bb3ba6340bac378b
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-10-22 12:38:41 -0500

    Created layout component, updated colors for bg and font (#4)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from '../helpers/themeContext';
-import { Toggle } from '../components/toggle';
+import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <Toggle />
-      <div className="mt-0 bg-primary text-primary">
-        <h1 className="text-6xl font-semibold">Hi, I'm Braydon</h1>
-        <h4 className="text-base">
+    <Layout>
+      <div>
+        <h1 className="text-6xl font-semibold text-primary">Hi, I'm Braydon</h1>
+        <h4 className="text-base text-secondary">
           I'm a senior full stack engineer with a passion for everything
           front-end
         </h4>
-        <div>some content here</div>
       </div>
-    </ThemeProvider>
+    </Layout>
   );
 }
