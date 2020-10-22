9ed56450953c6a5ccd40624f2db5462e772a52a0
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-10-22 17:54:03 -0500

    Feature/about section (#6)
    * Refactored out to presentational components, added about section
    
    * Refactored classnames

---
+++ src/components/about.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const AboutSection = () => (
+  <div className="space-y-4">
+    <h1 className="text-4xl font-semibold text-primary md:text-4xl">
+      About <mark>Me</mark>
+    </h1>
+    <div className="space-y-4 text-base font-thin text-secondary">
+      <p>
+        My name is Braydon Coyer. I’m a senior full stack engineer with a
+        passion for everything front-end.
+      </p>
+      <p>
+        As a visual person, one of the ways I communicate is by creating and
+        designing. That’s why I chose web development as a career. While I am a
+        full stack engineer, my true passion is front-end development. I love
+        the challenge of taking a mockup and translating it into a pixel-perfect
+        application. CSS is my obsession, and I always love learning new things
+        that can help in my quest to make the web a more user-friendly &
+        stunning place.
+      </p>
+      <p>
+        My career started at a consulting company that had a primary focus on
+        user experience and design, and stressed the importance of getting the
+        frontend ‘just right.’ While there, I developed an eye for great designs
+        that think of the end-user first.
+      </p>
+      <p>
+        Now years later, I’ve helped lead the development of several component
+        libraries for various clients, including Southwest Airlines, FedEx and
+        health care industry leaders.
+      </p>
+      <p>
+        I’m very active on CodePen and love creating little projects to share
+        and I am well versed in paired programming and test driven development.
+      </p>
+      <p>
+        I’m a firm believer that one never truly stops learning and I’m looking
+        to join a solid team and work on a product to help bring about change
+        and make a difference.
+      </p>
+      <p>
+        To put it simply, I have a passion for doing things like this. I would
+        love the opportunity to help make a difference in your project.
+      </p>
+      <p>- Braydon</p>
+    </div>
+  </div>
+);
+
+export default AboutSection;
