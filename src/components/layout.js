8249e28e911c92f59563c0cd88ebccadb97d8edc
Braydon Coyer <braydon@coyer.net>
2020-10-22 17:46:36 -0500

    Refactored out to presentational components, added about section

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,8 +5,8 @@ import { Toggle } from './toggle';
 
 const Layout = ({ children }) => (
   <ThemeProvider>
-    <main className="max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex justify-center mx-auto">
-      <Toggle />
+    <main className="p-4 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex justify-center mx-auto md:p-0">
+      {/* <Toggle /> */}
       <div>{children}</div>
     </main>
   </ThemeProvider>
