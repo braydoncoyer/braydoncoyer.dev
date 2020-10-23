b08201955dbcb048f27e1e3a2f9aa0e9173121ad
Braydon Coyer <braydon@coyer.net>
2020-10-23 18:12:55 -0500

    Added timeline section, and added custom green color to tailwind.config

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '../components/layout';
 import WelcomeSection from '../components/welcome';
 import AboutSection from '../components/about';
+import TimelineSection from '../components/timeline';
 
 export default function Home() {
   return (
     <Layout>
       <WelcomeSection />
       <AboutSection />
+      <TimelineSection />
     </Layout>
   );
 }
