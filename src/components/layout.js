72d8f9bd2b02a5e905c05d2e50579aecb6a2e0b1
Braydon Coyer <braydon@coyer.net>
2020-10-23 17:04:15 -0500

    added react icons, transition for appearance toggle

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,8 +5,10 @@ import { Toggle } from './toggle';
 
 const Layout = ({ children }) => (
   <ThemeProvider>
-    <main className="p-4 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex justify-center mx-auto md:p-0">
-      {/* <Toggle /> */}
+    <main className="px-4 relative max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex justify-center mx-auto md:p-0">
+      <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-0">
+        <Toggle />
+      </div>
       <div>{children}</div>
     </main>
   </ThemeProvider>
