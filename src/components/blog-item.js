3f683297866c042a0d01816826a29d4ab1293a31
Braydon Coyer <braydon@coyer.net>
2020-10-26 18:10:26 -0500

    feature/blog-section: Added section, making API call to get articles, displaying first 4 articles on page

--- src/components/blog-item.js
+++ src/components/blog-item.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { IoMdThumbsUp } from 'react-icons/io';
 
 const BlogItem = ({ title, description, date, reactions, slug }) => (
