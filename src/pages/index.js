be2686ac07a3fc24034650ec50fb0460fe0c7ac4
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-10-29 15:27:16 -0500

    Feature/sticky socials (#15)
    * feature/sticky-socials: icons for desktop complete
    
    * feature/stick-socials: mobile icons complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import Layout from '../components/layout';
+import SocialIcons from '../components/social';
 import WelcomeSection from '../components/welcome';
 import AboutSection from '../components/about';
 import TimelineSection from '../components/timeline';
@@ -15,15 +16,17 @@ export default function Home() {
         <title>Braydon's Portfolio</title>
         <link rel="canonical" href="https://braydoncoyer.dev/" />
       </Helmet>
-      <Layout>
-        <WelcomeSection />
-        <div className="space-y-32">
-          <AboutSection />
-          <BlogSection />
-          <TimelineSection />
-          <MusicSection />
-        </div>
-      </Layout>
+      <SocialIcons>
+        <Layout>
+          <WelcomeSection />
+          <div className="space-y-32">
+            <AboutSection />
+            <BlogSection />
+            <TimelineSection />
+            <MusicSection />
+          </div>
+        </Layout>
+      </SocialIcons>
     </div>
   );
 }
