10cbcb1b9ab26110ebf4e758ae221707f39abb00
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-11-28 09:15:09 -0600

    Feature/text colors (#21)
    * Updating secondary text color for dark mode
    
    * Edited all colors for dark mode, improved toolbox to more accurately reflect designs
    
    * feature/text-colors: updated blog layout

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { HiCheck } from 'react-icons/hi';
 
 const TimelineSection = () => (
-  <div className="mb-10 sm:mb-16 md:mb-20">
-    <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-lightBlue-500 mb-3">
+  <div className="mb-10 sm:mb-16 md:mb-40">
+    <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-lightBlue-500 dark:text-lightBlue-400 mb-3">
       Timeline
     </h2>
     <p className="text-coolGray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-12">
-      Some highlights of my life.
+      Some important things to note.
     </p>
     {/* Year */}
     <div className="flex justify-center items-center space-x-4 mb-11">
       <h2 className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white">
         2020
       </h2>
-      <div className="border-b-2 w-full text-coolGray-500" />
+      <div className="border-b-2 w-full border-coolGray-500 dark:border-coolGray-400" />
     </div>
     {/* Item */}
     <div className="flex items-start text-base sm:text-lg lg:text-base xl:text-lg ml-4 mb-7 max-w-4xl">
@@ -25,7 +25,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Launched updated portfolio 🙌
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           Launched my new portfolio powered by Gatsby, styled with Tailwind CSS
           and deployed on Netlify.
         </p>
@@ -40,7 +40,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Started a blog ✏️
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           In October, I decided it was time to start my development blog. I had
           been putting it off for years and finally made the jump. You can read
           my most recent articles here on this site!
@@ -56,7 +56,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Got hitched 👰
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           After dating for 4 years, I married the woman I've loved since
           middle-school.
         </p>
@@ -71,7 +71,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Started working from home 👨‍💻
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           In March we started working from home. Who knew it would last this
           long?
         </p>
@@ -83,7 +83,7 @@ const TimelineSection = () => (
       <h2 className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white">
         2019
       </h2>
-      <div className="border-b-2 w-full text-coolGray-500" />
+      <div className="border-b-2 w-full border-coolGray-500 dark:border-coolGray-400" />
     </div>
 
     {/* Item */}
@@ -95,7 +95,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Health care AI 🧰
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           I was placed in a team to architect and lead their front-end team to
           produce a component library for several health care industry leaders.
         </p>
@@ -111,7 +111,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Launched NgLimeade 🍞
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           Launched an Angular toast library to get toast notifications up and
           running in a project within 30 seconds.
         </p>
@@ -127,7 +127,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Started new job at Cognizant 🔧
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           Joined Cognizant in May to help lead their front-end practice in the
           Dallas Studios.
         </p>
@@ -143,7 +143,7 @@ const TimelineSection = () => (
         <p className="text-coolGray-900 dark:text-white font-semibold">
           Got engaged 💍
         </p>
-        <p className="text-coolGray-500 mt-1">
+        <p className="text-coolGray-500 dark:text-coolGray-400 mt-1">
           I asked my favorite person to marry me. She said yes!
         </p>
       </div>
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,20 +4,23 @@ import { SiHashnode } from 'react-icons/si';
 import Memoji from '../assets/braydon_memoji.jpeg';
 
 const WelcomeSection = () => (
-  <div className="min-h-screen flex justify-center items-center">
+  <div className="min-h-screen flex justify-between items-center">
     <div className="flex flex-col items-center space-y-8 md:flex-row-reverse md:justify-between">
-      <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" />
-      <div className="space-y-2 text-left">
+      {/* <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" /> */}
+      <div className="md:text-left text-center">
+        <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-teal-500 dark:text-teal-400 mb-3">
+          Hey there,
+        </h2>
         <h1 className="text-coolGray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-8">
-          Hi, I'm Braydon 👋
+          I'm Braydon.
         </h1>
-        <p className="text-coolGray-500 max-w-4xl text-lg sm:text-2xl font-normal sm:leading-10 space-y-6 mb-6">
+        <p className="text-coolGray-500 dark:text-coolGray-400 max-w-4xl text-lg sm:text-2xl font-normal sm:leading-10 space-y-6 mb-6">
           I'm a senior full stack engineer <br />
-          with a passion for everything front-end
+          with a passion for everything front-end.
         </p>
       </div>
 
-      <div className="w-full flex justify-evenly items-center text-xl text-coolGray-500 md:hidden">
+      <div className="w-full flex justify-evenly items-center text-xl text-coolGray-500 dark:text-coolGray-400 md:hidden">
         <a
           href="https://twitter.com/BraydonCoyer"
           target="_blank"
