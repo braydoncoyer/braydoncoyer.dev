ddd978c14e47136fb03ef4cde4d9606ecba9b221
Braydon Coyer <braydon@coyer.net>
2020-11-28 21:24:08 -0600

    added heart icon, messed with spacing

--- src/components/marquee.js
+++ src/components/marquee.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { FaRegHeart } from 'react-icons/fa';
+import { BsFillHeartFill } from 'react-icons/bs';
 import Marquee from 'react-marquee-slider';
+import heart from '../assets/heart.svg';
 
 const HeartIcon = () => (
   <div className="w-12 h-12 rounded-xl bg-gradient-to-br flex items-center justify-center from-pink-500 to-rose-500 mb-1">
-    <FaRegHeart className="text-lg" />
+    <img src={heart} alt="heart icon" className="h-8 w-8 animate-pulse" />
   </div>
 );
 
@@ -45,7 +46,7 @@ const MarqueeSection = () => {
           </h1>
         ))}
       </Marquee>
-      <div className="w-full border-b-2 mt-6 border-coolGray-500 dark:border-coolGray-400" />
+      <div className="w-full border-b-2 mt-7 border-coolGray-500 dark:border-coolGray-400" />
     </div>
   );
 };
