08947e2d3031f1b518769a6212d9e70e16cdcdfe
Braydon Coyer <braydon@coyer.net>
2020-12-02 21:23:34 -0600

    refactor/timeline: fixed some errors, brought memoji back to welcome screen

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,7 +6,7 @@ import Memoji from '../assets/braydon_memoji.jpeg';
 const WelcomeSection = () => (
   <div className="min-h-screen flex justify-between items-center">
     <div className="flex flex-col items-center space-y-8 md:flex-row-reverse md:justify-between">
-      {/* <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" /> */}
+      <img className="w-1/3 md:w-1/4" src={Memoji} alt="Memoji" />
       <div className="md:text-left text-center">
         <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-teal-500 dark:text-teal-400 mb-3">
           Hey there,
