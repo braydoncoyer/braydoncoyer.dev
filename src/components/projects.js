c7f5685995e4eb66958fbd7773a3c23441d15074
Braydon Coyer <braydon@coyer.net>
2020-12-03 22:51:27 -0600

    feature/projects: added macbook bundle pic

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BundleImage from '../assets/bundleappsmacbookpro.png';
 
 const ProjectsSection = () => (
   <div className="mb-16 sm:mb-24 md:mb-40">
@@ -10,7 +11,7 @@ const ProjectsSection = () => (
       <p className="mb-8">Check them out.</p>
     </div>
 
-    <ul className="grid grid-cols-1 gap-8 md:gap-10 md:grid-cols-3 md:grid-row-3">
+    {/* <ul className="grid grid-cols-1 gap-8 md:gap-10 md:grid-cols-3 md:grid-row-3">
       <li className="bg-coolGray-100 dark:bg-blueGray-800 md:col-span-2 rounded-3xl">
         <div className="h-96 p-10">
           <p>Item 1</p>
@@ -29,6 +30,69 @@ const ProjectsSection = () => (
       <li className="bg-coolGray-100 dark:bg-blueGray-800 rounded-3xl md:col-span-2">
         <div className="h-96 p-10">
           <p>Item 4</p>
+        </div>
+      </li>
+    </ul> */}
+
+    <ul className="grid grid-cols-1 gap-8 md:gap-10 md:grid-cols-2 md:grid-row-2">
+      <li className="bg-coolGray-100 dark:bg-blueGray-800 md:row-span-2 rounded-3xl">
+        <div className="p-10">
+          <div className="mb-14 md:mb-28">
+            <p className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white mb-2">
+              bundleapps.io
+            </p>
+            <p className="text-coolGray-500 dark:text-coolGray-400 text-lg sm:text-2xl font-normal sm:leading-10 space-y-6 mb-2">
+              A startup consultant company needed a website spun up quickly in
+              order to publish their first iOS application. Built with Tailwind
+              UI, I was able to put something together for them by the end of
+              the day.
+            </p>
+            <a href="https://bundleapps.io/" target="_blank" rel="noreferrer">
+              <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                <p>Visit -></p>
+              </div>
+            </a>
+          </div>
+          <div>
+            <img src={BundleImage} alt="bundleapps" />
+          </div>
+        </div>
+      </li>
+      <li className="bg-coolGray-100 dark:bg-blueGray-800 rounded-3xl">
+        <div className="h-96 p-10">
+          <p className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white mb-2">
+            OfficeAPI
+          </p>
+          <p className="text-coolGray-500 dark:text-coolGray-400 text-lg sm:text-2xl font-normal sm:leading-10 space-y-6 mb-2">
+            A fun side-project originally built with Spring Boot and MonogDb,
+            rebuilt with with Node.js, Express, Mongoose and MongoDb. Deployed
+            with Heroku.
+          </p>
+          <a href="https://www.officeapi.dev/" target="_blank" rel="noreferrer">
+            <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+              <p>Visit -></p>
+            </div>
+          </a>
+        </div>
+      </li>
+      <li className="bg-coolGray-100 dark:bg-blueGray-800 rounded-3xl">
+        <div className="h-96 p-10">
+          <p className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white mb-2">
+            NgLimeade
+          </p>
+          <p className="text-coolGray-500 dark:text-coolGray-400 text-lg sm:text-2xl font-normal sm:leading-10 space-y-6 mb-2">
+            NgLimeade is an Angular Toast Library that aims to get toast
+            notifications up and running in less than a minute! No joke!
+          </p>
+          <a
+            href="https://www.npmjs.com/package/ng-limeade"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+              <p>Visit -></p>
+            </div>
+          </a>
         </div>
       </li>
     </ul>
