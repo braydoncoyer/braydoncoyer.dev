e57ba235eaa774756fc34989c31427fb6f7bd48a
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2020-12-07 17:49:57 -0600

    feature/contentful-data: Interests and timeline is now sourced from Contentful (#30)

--- src/components/marquee.js
+++ src/components/marquee.js
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import Marquee from 'react-marquee-slider';
 import heart from '../assets/heart.svg';
@@ -8,29 +9,19 @@ const HeartIcon = () => (
   </div>
 );
 
-const interests = [
-  'Angular',
-  'React',
-  'Valorant',
-  'Apex Legends',
-  'Running',
-  'Gatsby',
-  'Panda Express',
-  'Dr. Pepper',
-  'Street Tacos',
-  'Apple Products',
-  'Basketball',
-  'CodePen',
-  'Test Driven Development',
-  'Public Speaking',
-  'Christmas Time',
-  'Lord of the Rings',
-  'Movie Music Scores',
-  'TailwindCSS',
-];
-
 const MarqueeSection = () => {
   const speed = 30;
+  const contentfulQuery = useStaticQuery(graphql`
+    {
+      allContentfulInterests {
+        edges {
+          node {
+            title
+          }
+        }
+      }
+    }
+  `);
   return (
     <section className="mb-24 md:mb-40">
       <div className="flex justify-between items-center">
@@ -40,14 +31,16 @@ const MarqueeSection = () => {
       </div>
 
       <Marquee velocity={speed}>
-        {interests.map((item, id) => (
-          <h1
-            key={id}
-            className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white ml-16"
-          >
-            {item}
-          </h1>
-        ))}
+        {contentfulQuery.allContentfulInterests.edges[0].node.title.map(
+          (item, id) => (
+            <h1
+              key={id}
+              className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white ml-16"
+            >
+              {item}
+            </h1>
+          )
+        )}
       </Marquee>
       <div className="w-full border-b-2 mt-7 border-coolGray-500 dark:border-coolGray-400" />
     </section>
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,72 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import TimelineYear from './timeline-year';
 
-const timelineyearItems = [
-  {
-    yearTitle: '2020',
-    yearItems: [
-      {
-        itemTitle: 'Launched updated portfolio 🙌',
-        description:
-          'Launched my new portfolio powered by Gatsby, styled with Tailwind CSS and deployed on Netlify.',
-      },
-      {
-        itemTitle: 'Started a blog ✏️',
-        description:
-          'In October, I decided it was time to start my development blog. I had been putting it off for years and finally made the jump. You can read my most recent articles here on this site!',
-      },
-      {
-        itemTitle: 'Got hitched 👰',
-        description:
-          "After dating for 4 years, I married the woman I've loved since middle-school!",
-      },
-      {
-        itemTitle: 'Started working from home 👨‍💻',
-        description:
-          'In March we started working from home. Who knew it would last this long?',
-      },
-    ],
-  },
+const TimelineSection = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulTimelineYear(sort: { fields: yearTitle, order: DESC }) {
+        edges {
+          node {
+            yearTitle
+            timelineItem {
+              itemTitle
+              description {
+                description
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
 
-  {
-    yearTitle: '2019',
-    yearItems: [
-      {
-        itemTitle: 'Health care AI 🧰',
-        description:
-          'I was placed in a team to architect and lead their front-end team to produce a component library for several health care industry leaders.',
-      },
-      {
-        itemTitle: 'Launched NgLimeade 🍞',
-        description:
-          'Launched an Angular toast library to get toast notifications up and running in a project within 30 seconds.',
-      },
-      {
-        itemTitle: 'Started new job at Cognizant 🔧',
-        description:
-          'Joined Cognizant in May to help lead their front-end practice in the Dallas Studios.',
-      },
-      {
-        itemTitle: 'Got engaged 💍',
-        description: 'I asked my favorite person to marry me. She said yes!',
-      },
-    ],
-  },
-];
+  useEffect(() => {
+    console.log('====TIMELINE DATA IS====');
+    console.log(data);
+  });
+  return (
+    <section className="mb-24 md:mb-40">
+      <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-lightBlue-500 dark:text-lightBlue-400 mb-3">
+        Timeline
+      </h2>
+      <p className="text-coolGray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-12">
+        Some important things to note.
+      </p>
 
-const TimelineSection = () => (
-  <section className="mb-24 md:mb-40">
-    <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-lightBlue-500 dark:text-lightBlue-400 mb-3">
-      Timeline
-    </h2>
-    <p className="text-coolGray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-12">
-      Some important things to note.
-    </p>
-
-    {timelineyearItems.map((year, id) => (
-      <TimelineYear key={id} timelineYear={year} />
-    ))}
-  </section>
-);
+      {data.allContentfulTimelineYear.edges.map((year, id) => (
+        <TimelineYear key={id} timelineYear={year} />
+      ))}
+    </section>
+  );
+};
 
 export default TimelineSection;
