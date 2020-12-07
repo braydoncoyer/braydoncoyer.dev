35ffab372ec1b02e2a3a9b07d710459a483f67f6
Braydon Coyer <braydon@coyer.net>
2020-12-07 00:09:38 -0600

    refactor/spacing: updated spacing between sections for small devices

--- src/helpers/toast.js
+++ src/helpers/toast.js
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react';
-import LimeadeToast from '../assets/limeadeToast.png';
+import { HiCheckCircle, HiExclamationCircle } from 'react-icons/hi';
 
-const ToastItem = () => {
+const ToastItem = ({ title, description, color, icon }) => {
   const [toastState, setToastState] = useState('enter');
 
   const handleToastClick = () => {
@@ -18,9 +18,24 @@ const ToastItem = () => {
       onClick={handleToastClick}
       className={`${
         toastState === 'enter' ? '' : 'translate-x-6 opacity-0'
-      } transform ease-out duration-300 cursor-pointer`}
+      } transform ease-out duration-300 cursor-pointer w-full`}
     >
-      <img src={LimeadeToast} alt="toast notification" />
+      {/* <img src={LimeadeToast} alt="toast notification" /> */}
+      <div className={`w-full bg-${color}-400 rounded-xl p-2`}>
+        <div className="w-full bg-white rounded-md px-4 py-2 flex items-center">
+          <div className={`text-${color}-400 text-5xl`}>
+            {icon === 'question' ? <HiCheckCircle /> : <HiExclamationCircle />}
+          </div>
+          <div>
+            <p className="ml-4 text-coolGray-900 font-semibold text-md tracking-tighter">
+              {title}
+            </p>
+            <p className="ml-4 text-coolGray-400 font-semibold text-md tracking-tighter">
+              {description}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
