602bbf226198a6141ba01f0b9cc46224f5f320e0
Braydon Coyer <braydon@coyer.net>
2020-12-07 22:42:10 -0600

    Trying to fix overflow issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
       </Helmet>
       <SocialIcons>
         <ThemeProvider>
-          <body className="bg-white dark:bg-coolGray-900 transition-all overflow-hidden">
+          <body className="bg-white dark:bg-coolGray-900 transition-all">
             <main>
               <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
                 <Toggle />
