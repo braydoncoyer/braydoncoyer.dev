0b71a23db31832618d7a6bc9ab1fe95bbebc355b
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-01-13 15:13:14 -0600

    Fix/toggle (#37)
    * Trying different things to fix bug
    
    * Basic fix implemented. Needs some refactoring...
    
    * Temp solution to mobile toggle issue
    
    * Removed blue highlight around images

--- src/helpers/themeContext.js
+++ src/helpers/themeContext.js
@@ -14,7 +14,7 @@ const getInitialTheme = () => {
   }
 
   // If you want to use light theme as the default, return "light" instead
-  return 'light';
+  return 'dark';
 };
 
 export const ThemeContext = React.createContext();
