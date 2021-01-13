9f2fcd1f62508f463102ed57aca3083dbfd4d296
Braydon Coyer <braydon@coyer.net>
2021-01-13 13:15:32 -0600

    Trying different things to fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
         <ThemeProvider>
           <body className="bg-white dark:bg-coolGray-900 transition-all">
             <main>
-              <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+              <div className=" bg-red-500 toggle">
                 <Toggle />
               </div>
               <Layout>
