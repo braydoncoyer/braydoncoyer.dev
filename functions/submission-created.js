d38e2422a1071a7ebdbfb22494a26211b634b531
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-01-14 21:35:54 -0600

    Feature/newsletter (#41)
    * Created form section, created event listener, created Netlify serverless function
    
    * updated functions to take in email from state
    
    * Removed querySelector
    
    * Formatting

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 
 const { EMAIL_TOKEN } = process.env;
-exports.handler = async (email) => {
-  console.log(`Recieved a submission: ${email}`);
+exports.handler = async (event) => {
+  const { payload } = JSON.parse(event.body);
+  console.log('payload: ', payload);
+  console.log(`Recieved a submission: ${payload.email}`);
+
   return fetch('https://api.buttondown.email/v1/subscribers', {
     method: 'POST',
     headers: {
       Authorization: `Token ${EMAIL_TOKEN}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({ email: payload.emailAddress }),
   })
     .then((response) => response.json())
     .then((data) => {
@@ -18,3 +21,19 @@ exports.handler = async (email) => {
     })
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
+
+// console.log(`Recieved a submission: ${email}`);
+// return fetch('https://api.buttondown.email/v1/subscribers', {
+//   method: 'POST',
+//   headers: {
+//     Authorization: `Token ${EMAIL_TOKEN}`,
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({ email }),
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(`Submitted to Buttondown:\n ${data}`);
+//   })
+//   .catch((error) => ({ statusCode: 422, body: String(error) }));
+// };
