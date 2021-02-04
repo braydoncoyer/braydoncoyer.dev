222c8725f67ccd5c54f9ea8b1cb425deff58940b
Braydon Coyer <braydon@coyer.net>
2021-02-04 09:43:43 -0600

    feature/component-composition: Layout component now encapsulates themeprovider, main and footer. All pages now simply use the Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,108 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 import Footer from './footer';
+import { ThemeProvider } from '../helpers/themeContext';
 import Nav from './nav';
 
-const Layout = ({ children }) => (
-  <div>
-    <Nav />
-    <div className="px-4 mx-auto max-w-screen-sm md:max-w-2xl">
-      {children}
-      <div className="mt-10 sm:mt-16 md:mt-20">
-        <Footer />
-      </div>
+const Layout = (props) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            image
+            keywords
+            siteUrl
+            twitterUsername
+          }
+        }
+      }
+    `
+  );
+
+  const { children, location, ...customMeta } = props;
+  const meta = {
+    title: 'Braydon Coyer - Portfolio',
+    description: 'Senior Full Stack Engineer, DOM Artist and TDD enthusiast.',
+    image: '',
+    url: site.siteUrl,
+    twitterUsername: site.twitterUsername,
+    type: 'website',
+    ...customMeta,
+  };
+
+  return (
+    <div>
+      <Helmet
+        title={meta.title}
+        meta={[
+          {
+            name: 'description',
+            content: meta.description,
+          },
+          {
+            name: 'og:title',
+            content: meta.title,
+          },
+          {
+            name: 'og:description',
+            content: meta.description,
+          },
+          {
+            name: 'og:type',
+            content: meta.type,
+          },
+          {
+            name: 'og:site_name',
+            content: 'Braydon Coyer',
+          },
+          {
+            name: 'og:image',
+            content: meta.image,
+          },
+          {
+            name: 'og:url',
+            content: meta.url,
+          },
+          {
+            name: 'twitter:card',
+            content: 'summary_large_image',
+          },
+          {
+            name: 'twitter:site',
+            content: meta.twitterUsername,
+          },
+          {
+            name: 'twitter:title',
+            content: meta.title,
+          },
+          {
+            name: 'twitter:description',
+            content: meta.description,
+          },
+          {
+            name: 'twitter:image',
+            content: meta.image,
+          },
+        ]}
+      />
+      <ThemeProvider>
+        <body className="bg-white dark:bg-coolGray-900">
+          <div className="min-h-screen px-4 mx-auto max-w-screen-sm md:max-w-2xl flex flex-col">
+            <Nav />
+            <main className="flex-grow">{children}</main>
+            <div className="mt-12">
+              <Footer />
+            </div>
+          </div>
+        </body>
+      </ThemeProvider>
     </div>
-  </div>
-);
+  );
+};
 
 export default Layout;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,16 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import SEO from '../helpers/seo';
-import { ThemeProvider } from '../helpers/themeContext';
 import Layout from '../components/layout';
-import Banner from '../../static/images/NewsletterBanner.png';
 import AboutSection from '../components/about';
 import MarqueeSection from '../components/marquee';
 
 export default function Toolbox() {
   return (
     <>
-      <SEO title="Braydon's Portfolio" image={Banner} />
-      <ThemeProvider>
-        <body className="bg-white dark:bg-coolGray-900 min-h-screen">
-          <main>
-            <Layout>
-              <AboutSection />
-              <MarqueeSection />
-            </Layout>
-          </main>
-        </body>
-      </ThemeProvider>
+      <Layout title="About | Braydon Coyer" description="Here's my story.">
+        <AboutSection />
+        <MarqueeSection />
+      </Layout>
     </>
   );
 }
