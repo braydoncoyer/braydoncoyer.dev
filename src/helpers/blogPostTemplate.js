a7ae640cf5aa3e47091340a00848bed1a01352f7
Braydon Coyer <braydon@coyer.net>
2021-02-07 16:48:33 -0600

    Updated blogPostTemplate component to render mdx content

--- src/helpers/blogPostTemplate.js
+++ src/helpers/blogPostTemplate.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
 
-export default () => (
-  <>
-    <p>Blog post goes here...</p>
-  </>
-);
+const BlogPostTemplate = ({ data }) => {
+  const { frontmatter, body } = data.mdx;
+
+  return (
+    <>
+      <Layout>
+        <h1>{frontmatter.title}</h1>
+        <p>{frontmatter.publishedAt}</p>
+        <MDXRenderer>{body}</MDXRenderer>
+      </Layout>
+    </>
+  );
+};
+
+export const query = graphql`
+  query PostBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        publishedAt
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPostTemplate;
