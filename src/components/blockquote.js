9e7cde7ae97ee9a61ddb99e7341f3f55ed29df49
Braydon Coyer <braydon@coyer.net>
2021-02-09 22:27:40 -0600

    Adding appropriate bottom margin for blockquote and thoughtquote

--- src/components/blockquote.js
+++ src/components/blockquote.js
@@ -10,7 +10,7 @@ const styles = {
 };
 
 const Blockquote = ({ content }) => (
-  <div className="px-8 py-1 mb-12 border-l-4 leading-relaxed dark:text-coolGray-300 relative border-emerald-500 bg-emerald-500 bg-opacity-10">
+  <div className="px-8 py-1 mb-6 border-l-4 leading-relaxed dark:text-coolGray-300 relative border-coolGray-600 dark:border-coolGray-300 bg-coolGray-400 bg-opacity-10">
     <div
       className="text-center bg-white dark:bg-coolGray-900 rounded-full w-10 h-10 flex items-center justify-center"
       style={styles}
--- src/components/thoughtquote.js
+++ src/components/thoughtquote.js
@@ -10,7 +10,7 @@ const styles = {
 };
 
 const Thoughtquote = ({ content }) => (
-  <div className="px-8 py-1 mb-12 border-l-4 leading-relaxed dark:text-coolGray-300 relative border-blue-500 bg-blue-500 bg-opacity-10">
+  <div className="px-8 py-1 mb-6 border-l-4 leading-relaxed dark:text-coolGray-300 relative border-blue-500 bg-blue-500 bg-opacity-10">
     <div
       className="text-center bg-white dark:bg-coolGray-900 rounded-full w-10 h-10 flex items-center justify-center"
       style={styles}
