409a3e355a397bf00bdb003df0def8d0a4cc0c4d
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-02-14 00:15:00 -0600

    Feature/mdx blog (#61)
    * feature/mdx-blog: added dummy blog posts, dynamically creating pages based on query for markdown posts
    
    * Updated blogPostTemplate component to render mdx content
    
    * Commenting out inline code to remove linting errors
    
    * Gatsby image working for blog post cover image
    
    * Added cite banner, and moved images into folders for each article
    
    * Changed node creation path to append to /blog/
    
    * Added mdx support for cite article and dynamically using blockquote and thoughtquote components
    
    * Adding appropriate bottom margin for blockquote and thoughtquote
    
    * Added prism highlighting, made Toggle actual semantic button, altered blog article spacing on blog page
    
    * commented out mapping over filtered blog posts
    
    * Added custom code block theme css file, added code block language css file, updated thoughtquote to render children, fixed spacing and color issues in blogPostTemplate component
    
    * Highlighting code lines implemented, discuss on twitter feature added to blog posts
    
    * Added useSiteMetadata hook and added SEO component to separate pages and blog posts
    
    * Added default sharing image if article does not have article image from markdown
    
    * Added github blog articles, removed blog search bar, removed view count
    
    * Removing unused variables
    
    * Updated cite tag blog post
    
    * Added imageName property to mdx to properly fix SEO sharing images
    
    * Added slash for correct social image sharing
    
    * Created article view count with firebase

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import './src/styles/global.css';
 import './src/styles/prism-theme.css';
+import 'firebase/database';
 import { wrapRootElement as wrap } from './root-wrapper';
 
 export const wrapRootElement = wrap;
--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,20 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-firebase',
+      options: {
+        credentials: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          databaseURL: process.env.DATABASE_URL,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/data/blog`,
