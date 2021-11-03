e97ef6188b7ed2f3b169e1bc45b2de4b352aa909
Braydon Coyer <braydoncoyer@Braydons-M1-MBP.attlocal.net>
2021-11-03 16:55:23 -0500

    Added tag styles to blog page. Search bar styled. Images have slight border radius

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,12 +1,10 @@
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
 import { convertToArticleList, getPublishedArticles } from '@/lib/notion';
 import { useEffect, useState } from 'react';
 
-import { ArticleCard } from '@/components/ArticleCard';
 import { ArticleList } from '@/components/ArticleList';
-import { Client } from '@notionhq/client';
 import { Container } from 'layouts/Container';
-import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { Tag } from '@/components/Tag';
 
 export default function Blog({ articles, tags }) {
   const [selectedTag, setSelectedTag] = useState<string>('');
@@ -29,24 +27,24 @@ export default function Blog({ articles, tags }) {
     <Container>
       <div>
         <input
+          className="bg-white dark:bg-midnight py-3 px-8 shadow-sm focus:ring-midnight dark:focus:ring-gray-100 block w-full sm:text-sm md:text-lg border-gray-300 dark:border-midnight-hover rounded-full"
           type="text"
           onChange={(e) => setSearchValue(e.target.value)}
           value={searchValue}
-          placeholder="Search articles"
+          placeholder={`Search ${articles.length} articles`}
         />
       </div>
-      <h2>Tags</h2>
-      <ul className="flex items-center justify-start flex-wrap space-x-4 list-none !important">
-        {tags &&
-          tags.map((tag) => (
-            <button key={tag} onClick={() => setSelectedTag(tag)}>
-              {tag}
-            </button>
-          ))}
-      </ul>
-      <h2>Articles</h2>
-      {!filteredArticles.length && <p>No articles found.</p>}
-      <ArticleList articles={filteredArticles} />
+      <div className="space-y-12 min-h-screen">
+        <h3>Discover articles by topic</h3>
+        <ul className="flex items-center justify-start flex-wrap list-none !important">
+          {tags &&
+            tags.map((tag) => (
+              <Tag key={tag} tag={tag} cb={() => setSelectedTag(tag)} />
+            ))}
+        </ul>
+        {!filteredArticles.length && <p>No articles found.</p>}
+        <ArticleList articles={filteredArticles} />
+      </div>
     </Container>
   );
 }
