d6c40b9c51d05da30f73b02e9b8df5b0f0317bae
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-11-29 17:05:49 -0600

    Added project page with four highlights (#129)

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,13 +14,13 @@ export const Button: React.FC<Props> = ({
     <button
       aria-label="button"
       type="button"
-      className={`w-full md:w-auto md:inline-flex py-3 px-12 rounded-full ${
+      className={`md:w-auto md:inline-flex py-3 px-12 rounded-full w-full ${
         buttonType === ButtonType.PRIMARY
           ? 'bg-midnight text-white dark:bg-gray-200 dark:text-midnight'
           : buttonType === ButtonType.SECONDARY
           ? 'bg-gray-200 dark:bg-midnight text-midnight dark:text-white'
           : null
-      } items-center justify-center general-ring-state`}
+      } items-center justify-center general-ring-state font-medium`}
       onClick={() => onButtonClick()}
     >
       {children}
