dfd6c74adb55c32ab571658a65ad96e5bd5007ed
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-12-07 08:35:36 -0600

    Added animation via framer motion (#135)

---
+++ components/PageTransition.tsx
@@ -0,0 +1,33 @@
+import { motion, useReducedMotion } from 'framer-motion';
+
+import { ReactNode } from 'react';
+
+const variants = {
+  initial: {
+    opacity: 0,
+    y: 8
+  },
+  enter: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      ease: [0.61, 1, 0.88, 1]
+    }
+  }
+};
+
+export const PageTransition = ({
+  children
+}: {
+  children: ReactNode;
+}): JSX.Element => {
+  const prefersReducedMotion = useReducedMotion();
+
+  if (prefersReducedMotion) return <>{children}</>;
+  return (
+    <motion.div initial="initial" animate="enter" variants={variants}>
+      {children}
+    </motion.div>
+  );
+};
