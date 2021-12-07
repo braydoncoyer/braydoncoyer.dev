f5dadcbf1cc9d03d1920662ea8cbe22417baa01e
Braydon Coyer <38799309+braydoncoyer@users.noreply.github.com>
2021-12-07 09:04:54 -0600

    Added plausible provider (#136)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import 'styles/globals.css';
 import 'styles/codeblocks.css';
 
 import type { AppProps } from 'next/app';
+import PlausibleProvider from 'next-plausible';
 import { ThemeProvider } from 'next-themes';
+import siteMetadata from '@/data/siteMetadata';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider domain={siteMetadata.siteUrl} trackOutboundLinks={true}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
 
