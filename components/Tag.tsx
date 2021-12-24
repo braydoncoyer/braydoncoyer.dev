7bcc9f9d11713fac4ec9a7f2ebf754824489fcd2
Saurabh <79586784+saurabhggc@users.noreply.github.com>
2021-12-24 20:48:59 +0530

    Update Tag.tsx

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,8 +3,8 @@ export function Tag({ tag, cb, activeTag }) {
     <button
       onClick={() => cb()}
       className={`mr-4 rounded-full px-6 py-1 ${
-        activeTag === tag && 'border border-teal-500 text-teal-500'
-      } hover:border hover:border-gray-300 `}
+        activeTag === tag && 'ring-2 ring-teal-500 text-teal-500'
+      } hover:ring-2 hover:ring-gray-300 `}
     >
       <span className="text-base font-medium uppercase">
         {tag === '' ? 'all' : tag}
