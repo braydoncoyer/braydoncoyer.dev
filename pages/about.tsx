2926556143c93dd67c74f6e5e8196c637fee4786
Braydon Coyer <braydon@coyer.net>
2022-01-09 17:17:55 -0600

    Updated phrasing of blog area.

--- pages/about.tsx
+++ pages/about.tsx
@@ -128,7 +128,7 @@ export default function About({ recentArticles }) {
       </div>
       <hr className="my-16 w-full border-none text-center h-10 before:content-['∿∿∿'] before:text-[#D1D5DB] before:text-2xl"></hr>
       <div className="mb-12">
-        <h2>I love to share my knowledge by writing.</h2>
+        <h2>I love to share my knowledge through writing.</h2>
         <p>Check out a few of my most recent publishings.</p>
         <ArticleList articles={recentArticles} />
       </div>
