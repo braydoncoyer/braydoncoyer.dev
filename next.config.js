9e36ef706b799be0c8324d4ce7e20bc775bf1d0e
Saurabh <79586784+saurabhggc@users.noreply.github.com>
2022-02-25 00:47:42 +0530

    Replaced `react` with `preact` in production bundle (#150)

--- next.config.js
+++ next.config.js
@@ -3,20 +3,34 @@ module.exports = {
     return [
       {
         source: '/sitemap.xml',
-        destination: '/api/sitemap'
-      }
+        destination: '/api/sitemap',
+      },
     ];
   },
   images: {
     domains: [
       's3.us-west-2.amazonaws.com', // Images coming from Notion
       'via.placeholder.com', // for articles that do not have a cover image
-      'images.unsplash.com', // For blog posts that use an external cover image
+      'images.unsplash.com', // For blog posts that use an external cover ima ge
       'pbs.twimg.com', // Twitter Profile Picture
       'dwgyu36up6iuz.cloudfront.net',
       'cdn.hashnode.com',
       'res.craft.do',
-      'res.cloudinary.com'
-    ]
-  }
+      'res.cloudinary.com',
+    ],
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build.
+    // With this you ship way less javascript to the client I mean almost half.
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime',
+      });
+    }
+
+    return config;
+  },
 };
