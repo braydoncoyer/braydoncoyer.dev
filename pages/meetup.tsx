d5bb19921c09f08f4b14528e4fb929b8aa3d3ba6
Braydon Coyer <braydon@coyer.net>
2022-05-30 10:39:29 -0500

    Created a list of people I would love to meet face to face

--- pages/meetup.tsx
+++ pages/meetup.tsx
@@ -5,22 +5,31 @@ import { GetStaticProps } from 'next';
 import { StickyColumn } from 'layouts/StickyColumn';
 import { getPageInfo } from '@/lib/notion';
 import { renderBlocks } from '@/lib/renderBlocks';
+import siteMetadata from '@/data/siteMetadata';
 
-export default function Toolbox({ toolboxContent }) {
+export default function Toolbox({ meetupContent }) {
   return (
-    <Container>
+    <Container title="Meetup - Braydon Coyer">
       <h1>
         <span className="block text-base font-semibold tracking-wide text-center text-teal-500 uppercase dark:text-teal-400">
-          Toolbox
+          Meetup
         </span>
         <span className="block max-w-2xl mx-auto mt-2 text-4xl font-bold leading-10 text-center sm:text-5xl">
-          Here's what I use daily.
+          A list of people I would love to meet in real life.
         </span>
       </h1>
+      <p>
+        The internet provides wonderful opportunities to be introduced to
+        amazing people. Here's a list of individuals I would love to meet face
+        to face! Want to meet up or be added to the list? Send me a{' '}
+        <a target="_blank" href={siteMetadata.twitter} rel="noreferrer">
+          DM on Twitter!
+        </a>
+      </p>
       <StickyColumn>
-        <div className="col-span-8 mt-12">
-          <div className="space-y-16">
-            {toolboxContent.map((block) => (
+        <div className="mt-12">
+          <div className="grid grid-cols-2">
+            {meetupContent.map((block) => (
               <Fragment key={block.id}>{renderBlocks(block)}</Fragment>
             ))}
           </div>
@@ -31,11 +40,11 @@ export default function Toolbox({ toolboxContent }) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await getPageInfo(process.env.TOOLBOX_PAGE_ID);
+  const data = await getPageInfo(process.env.MEETUP_DATABASE_ID);
 
   return {
     props: {
-      toolboxContent: data
+      meetupContent: data
     },
     revalidate: 1800
   };
