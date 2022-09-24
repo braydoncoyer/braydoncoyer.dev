d382d5e0128f3262b627da0255b0a44e36d9699c
Braydon Coyer <braydon@coyer.net>
2022-09-24 14:20:18 -0500

    Added back next config, but removed webpage object

--- next.config.js
+++ next.config.js
@@ -1,1 +1,24 @@
-module.exports = {};
+module.exports = {
+  async redirects() {
+    return [
+      // portfolio archives
+      {
+        source: '/archives/v1',
+        destination: 'https://wizardly-payne-b3707b.netlify.app/',
+        permanent: true
+      }
+    ];
+  },
+  images: {
+    domains: [
+      's3.us-west-2.amazonaws.com', // Images coming from Notion
+      'via.placeholder.com', // for articles that do not have a cover image
+      'images.unsplash.com', // For blog posts that use an external cover ima ge
+      'pbs.twimg.com', // Twitter Profile Picture
+      'dwgyu36up6iuz.cloudfront.net',
+      'cdn.hashnode.com',
+      'res.craft.do',
+      'res.cloudinary.com'
+    ]
+  }
+};
