e7928d58bada777d12bdc685cf1b77086706a1e6
Braydon Coyer <braydon@coyer.net>
2022-09-24 14:22:21 -0500

    adding back webpack

--- next.config.js
+++ next.config.js
@@ -20,5 +20,19 @@ module.exports = {
       'res.craft.do',
       'res.cloudinary.com'
     ]
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build.
+    // With this you ship way less javascript to the client I mean almost half.
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime'
+      });
+    }
+
+    return config;
   }
 };
