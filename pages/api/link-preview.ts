249a63549848529612c38ec2d1272b1a30a27ddf
Braydon Coyer <braydon@coyer.net>
2022-12-10 11:58:43 -0600

    added puppeteer again

--- pages/api/link-preview.ts
+++ pages/api/link-preview.ts
@@ -1,5 +1,4 @@
-import chromium from 'chrome-aws-lambda';
-import playwright from 'playwright-core';
+import puppeteer from 'puppeteer';
 
 export default async function handler(req, res) {
   try {
@@ -20,28 +19,10 @@ export default async function handler(req, res) {
 }
 
 const getImageBase64 = async (url) => {
-  const browser = await playwright.chromium.launch({
-    args: [...chromium.args, '--font-render-hinting=none'], // This way fix rendering issues with specific fonts
-    executablePath:
-      process.env.NODE_ENV === 'production'
-        ? await chromium.executablePath
-        : '/opt/homebrew/bin/chromium',
-    headless: process.env.NODE_ENV === 'production' ? chromium.headless : true
-  });
-
-  const context = await browser.newContext();
-
-  const page = await context.newPage();
-
-  // let page = await browser.newPage();
-
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
   await page.goto(url, { waitUntil: 'load' });
-
-  let image = await page.screenshot();
-
-  image.toString('base64');
-
+  let image = await page.screenshot({ encoding: 'base64' });
   await browser.close();
-
   return image;
 };
