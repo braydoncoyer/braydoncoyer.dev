577e329cfe5c1c42f1868dbf8c7dba943d3518ed
Braydon Coyer <braydon@coyer.net>
2024-12-30 08:54:21 -0600

    Fixed supabase type errors

--- app/lib/supabase/server.ts
+++ app/lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export default async function createSupabaseServerClient() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
