07049e57c23fea1fd9130c9599d2d3bb4b4ccf2b
Braydon Coyer <braydon@coyer.net>
2026-01-01 15:25:51 -0600

    Trying to fix dynamic OG generation

--- middleware.ts
+++ middleware.ts
@@ -66,8 +66,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
+     * - Static assets (images, etc.)
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|ico|webp)$).*)",
   ],
 };
